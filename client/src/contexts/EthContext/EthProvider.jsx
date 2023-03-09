a73b86e37c1804b98d385364af8ff7f004424714
Kevin Tesson <111077078+ShardWei@users.noreply.github.com>
2023-03-09 12:32:43 +0100

    remplace voter.json by Voting.json

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -30,7 +30,7 @@ function EthProvider({ children }) {
   useEffect(() => {
     const tryInit = async () => {
       try {
-        const artifact = require("../../contracts/Voter.json");
+        const artifact = require("../../contracts/Voting.json");
         init(artifact);
       } catch (err) {
         console.error(err);
