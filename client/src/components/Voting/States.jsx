0e081dea9dd590094de25968f20aeff38a88a627
LEL <lel@example.com>
2023-03-11 20:55:20 +0100

    Add States functions

--- client/src/components/Voting/States.jsx
+++ client/src/components/Voting/States.jsx
@@ -1,8 +1,131 @@
+import { useState, useEffect } from "react";
+import useEth from "../../contexts/EthContext/useEth";
+
 function States() {
+  const { state: { contract, accounts } } = useEth();
+  const [currentWorkflowStatus, setCurrentWorkflowStatus] = useState();
+  const [workflowStatusLog, setWorkflowStatusLog] = useState();
+  const [workflowEvents, setWorkflowEvents] = useState();
+ 
+  //Show current status
+  useEffect(() => {
+    (async function () {
+      const workflowStatus= await contract.methods.workflowStatus().call();
+      setCurrentWorkflowStatus(workflowStatus);
+
+    })();
+  }, [contract,workflowStatusLog])
+
+
+  //show status event history
+  useEffect(() => {
+    (async function () {
+      const workflowStatusEvents= await contract.getPastEvents('WorkflowStatusChange', {fromBlock: 0,toBlock: 'latest'});
+      const workflowChanges=[];
+
+      workflowStatusEvents.forEach(event => {
+        const eventChange = ("Change status from " + event.returnValues.previousStatus + " to " + event.returnValues.newStatus)
+        workflowChanges.push( eventChange);
+      });
+
+      //Build a list of <li>event</li>
+      const listEvents = workflowChanges.map((event,index) => <li key={"status"+index}>{event}</li>);
+      setWorkflowEvents(listEvents);
+ 
+    })();
+  }, [contract,workflowStatusLog])
+
+
+
+
+
+  //Change workflowstatus to ProposalsRegistrationStarted
+  const startProposals = async () => { 
+      if (await contract.methods.startProposalsRegistering().call({ from: accounts[0] })){
+      const workflowStatusTx =  await contract.methods.startProposalsRegistering().send({ from: accounts[0] })
+
+      const workflowPreviousStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.previousStatus;
+      const workflowNewStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.newStatus;
+
+      setWorkflowStatusLog ("Change status from " + workflowPreviousStatus + " to " + workflowNewStatus);
+    }
+  };
+
+  //Change workflowstatus to ProposalsRegistrationEnded
+  const endProposals = async () => { 
+    if (await contract.methods.endProposalsRegistering().call({ from: accounts[0] })){
+      const workflowStatusTx =  await contract.methods.endProposalsRegistering().send({ from: accounts[0] })
+
+      const workflowPreviousStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.previousStatus;
+      const workflowNewStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.newStatus;
+
+      setWorkflowStatusLog ("Change status from " + workflowPreviousStatus + " to " + workflowNewStatus);
+    }
+  };
+
+  //Change workflowstatus to VotingSessionStarted
+  const startVoting = async () => { 
+    if (await contract.methods.startVotingSession().call({ from: accounts[0] })){
+    const workflowStatusTx =  await contract.methods.startVotingSession().send({ from: accounts[0] })
+
+    const workflowPreviousStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.previousStatus;
+    const workflowNewStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.newStatus;
+
+    setWorkflowStatusLog ("Change status from " + workflowPreviousStatus + " to " + workflowNewStatus);
+    }
+  };
+
+  //Change workflowstatus to VotingSessionEnded
+  const endVoting = async () => { 
+    if (await contract.methods.endVotingSession().call({ from: accounts[0] })){
+        const workflowStatusTx =  await contract.methods.endVotingSession().send({ from: accounts[0] })
+
+        const workflowPreviousStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.previousStatus;
+        const workflowNewStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.newStatus;
+
+        setWorkflowStatusLog ("Change status from " + workflowPreviousStatus + " to " + workflowNewStatus);
+    }
+  };
+
+  //Change workflowstatus to VotesTallied
+  const tallyVotes = async () => { 
+
+    if (await contract.methods.tallyVotes().call({ from: accounts[0] })){
+      const workflowStatusTx =  await contract.methods.tallyVotes().send({ from: accounts[0] })
+
+      const workflowPreviousStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.previousStatus;
+      const workflowNewStatus = workflowStatusTx.events.WorkflowStatusChange.returnValues.newStatus;
+
+      setWorkflowStatusLog ("Change status from " + workflowPreviousStatus + " to " + workflowNewStatus);
+    }
+  };
+
+
 
   return (
     <div className="states">
       <h3>States</h3>
+      <div>
+      <span>Current workflow status : </span><span>{currentWorkflowStatus}</span>
+      </div>
+      <div>
+        <p>Change workflow status actions :</p>
+        {
+          (currentWorkflowStatus === "0") ? <button onClick={startProposals}>Start propossals registration</button> :
+          (currentWorkflowStatus === "1") ? <button onClick={endProposals}>End propossals registration</button> :
+          (currentWorkflowStatus === "2") ? <button onClick={startVoting}>Start voting session</button> :
+          (currentWorkflowStatus === "3") ? <button onClick={endVoting}>End voting session</button> :
+          (currentWorkflowStatus === "4") ? <button onClick={tallyVotes}>Tally votes</button> :
+          <span>Vote Session Finished</span>
+        }   
+      </div>
+      <div>
+        <span>Logs :</span><span>{workflowStatusLog}</span>
+      </div>
+      <div>
+        <span>Workflows status history :</span>
+        <ul>{workflowEvents}</ul>
+      </div>
     </div>
   );
 }
