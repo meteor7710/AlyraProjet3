d5cf427143015398c2990a49fd96e444ce272fe1
Kevin Tesson <111077078+ShardWei@users.noreply.github.com>
2023-03-11 23:54:32 +0000

    add proposals result & votes components

--- client/src/components/Voting/Votes.jsx
+++ client/src/components/Voting/Votes.jsx
@@ -1,9 +1,45 @@
+import { useState, useEffect } from "react";
+import { useEth } from "../../contexts/EthContext";
+
 function Votes() {
+  const { state: { accounts, contract, artifact }} = useEth();
+  const [proposals, setVote] = useState([]);
+  const [hasVoted, setHasVoted] = useState(false);
+
+  useEffect(() => {
+
+    (async () => {
+      setVote(await getVote());
+    })();
+  }, []);
+
+  const handleClick = async (proposalId) => {
+    await proposals(proposalId);
+  }
 
   return (
     <div className="votes">
       <h3>Votes</h3>
+      <div>
+        <h4>Choose your favorite proposal !</h4>
+        <table>
+          <tbody>
+            {proposals.map((proposal) =>
+              <tr key={proposal.id}>
+                  <td>{proposal.id}</td>
+                  <td>{proposal.description}</td>
+                  <td>
+                      {(hasVoted && votedProposalId === proposal.id) && '√'}
+                      {(hasVoted && votedProposalId !== proposal.id) && ''}
+                      {!hasVoted && <button onSubmit={() => handleClick(proposal.id)}>add your Vote</button>}
+                  </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
+    
   );
 }
 
