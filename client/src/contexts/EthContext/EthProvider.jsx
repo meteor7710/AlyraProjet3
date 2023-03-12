1dac5f3cb57aa33ffe6dd53976713d0927b0d28c
LEL <lel@example.com>
2023-03-12 23:36:22 +0100

    add owner state

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -13,16 +13,17 @@ function EthProvider({ children }) {
         const accounts = await web3.eth.requestAccounts();
         const networkID = await web3.eth.net.getId();
         const { abi } = artifact;
-        let address, contract;
+        let address, contract,owner;
         try {
           address = artifact.networks[networkID].address;
           contract = new web3.eth.Contract(abi, address);
+          owner = await contract.methods.owner().call();
         } catch (err) {
           console.error(err);
         }
         dispatch({
           type: actions.init,
-          data: { artifact, web3, accounts, networkID, contract }
+          data: { artifact, web3, accounts, networkID, contract, owner }
         });
       }
     }, []);
