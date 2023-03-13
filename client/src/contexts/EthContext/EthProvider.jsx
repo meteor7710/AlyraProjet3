500b62c7d3ce3bf2d71f30d521ab236274773938
LEL <lel@example.com>
2023-03-13 18:55:29 +0100

    add comments for owner and creationBlock

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -17,8 +17,9 @@ function EthProvider({ children }) {
         try {
           address = artifact.networks[networkID].address;
           contract = new web3.eth.Contract(abi, address);
+          //Add owner state to filter views
           owner = await contract.methods.owner().call();
-
+          //Add contract creation block to reduce event queries
           const creationEvent = await contract.getPastEvents('OwnershipTransferred', {filter:{previousOwner:"0x0000000000000000000000000000000000000000"},fromBlock: 0,toBlock: 'latest'});
           creationBlock = creationEvent[0].blockNumber;
 
