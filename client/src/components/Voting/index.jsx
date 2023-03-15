57535513d415203f2bfa24ea9fd4dfaf5386bbfa
LEL <lel@example.com>
2023-03-15 23:21:08 +0100

    Show current address

--- client/src/components/Voting/index.jsx
+++ client/src/components/Voting/index.jsx
@@ -3,15 +3,24 @@ import Title from "./Title";
 import Owner from "./Owner";
 import Voter from "./Voter";
 import Result from "./Result";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NoticeNoArtifact from "./NoticeNoArtifact";
 import NoticeWrongNetwork from "./NoticeWrongNetwork";
 
 function Voting() {
   const { state } = useEth();
+  const { state: { accounts } } = useEth();
   const [addressToWhitelistLog, setAddressToWhitelistLog]= useState();
   const [workflowStatusLog, setWorkflowStatusLog] = useState();
   const [currentWorkflowStatus, setCurrentWorkflowStatus] = useState();
+  const [currentAddress, setCurrentAddress] = useState();
+
+  useEffect(() => {
+    (async function () {
+      setCurrentAddress(accounts[0])
+    })();
+  }, [accounts])
+
 
   const voting =
     <>
@@ -25,6 +34,7 @@ function Voting() {
   return (
     <div className="voting">
       <Title />
+      <span>Current Address : {currentAddress}</span>
       {
         !state.artifact ? <NoticeNoArtifact /> :
           !state.contract ? <NoticeWrongNetwork /> :
